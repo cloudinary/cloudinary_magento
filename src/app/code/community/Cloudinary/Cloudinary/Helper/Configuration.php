712bbaf9e0f43455b721e52fea0cd5445f5b7f4f
Marco Lopes <marco.paulo.lopes@gmail.com>
2014-10-21 11:32:47 +0100

    Add cloudinary API configuration to admin's system configuration
    Conflicts:
    	src/app/code/community/Cloudinary/Cloudinary/etc/adminhtml.xml
    	src/app/code/community/Cloudinary/Cloudinary/etc/config.xml
    	src/app/code/community/Cloudinary/Cloudinary/etc/system.xml

---
+++ src/app/code/community/Cloudinary/Cloudinary/Helper/Configuration.php
@@ -0,0 +1,14 @@
+<?php
+ 
+class Cloudinary_Cloudinary_Helper_Configuration extends Mage_Core_Helper_Abstract {
+
+    public function getApiKey()
+    {
+        return Mage::helper('core')->decrypt(Mage::getStoreConfig('cloudinary/credentials/cloudinary_api_key'));
+    }
+
+    public function getApiSecret()
+    {
+        return Mage::helper('core')->decrypt(Mage::getStoreConfig('cloudinary/credentials/cloudinary_api_secret'));
+    }
+}
