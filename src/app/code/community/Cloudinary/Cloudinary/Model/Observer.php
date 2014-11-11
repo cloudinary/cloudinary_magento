662b014ee296186c6c62608d4738575819f78d3c
Marco Lopes <marco.paulo.lopes@gmail.com>
2014-11-11 14:30:22 +0000

    SESSCLOUD-8: Use class constant for the regex converting classes to paths

--- src/app/code/community/Cloudinary/Cloudinary/Model/Observer.php
+++ src/app/code/community/Cloudinary/Cloudinary/Model/Observer.php
@@ -1,9 +1,10 @@
 <?php
- 
+
 class Cloudinary_Cloudinary_Model_Observer extends Mage_Core_Model_Abstract
 {
     const CLOUDINARY_EXTENSION_LIB_PATH = 'CloudinaryExtension';
     const CLOUDINARY_LIB_PATH = 'Cloudinary';
+    const CONVERT_CLASS_TO_PATH_REGEX = '#\\\|_(?!.*\\\)#';
 
     public function onFrontInitBefore(Varien_Event_Observer $event)
     {
@@ -34,7 +35,7 @@ class Cloudinary_Cloudinary_Model_Observer extends Mage_Core_Model_Abstract
                     strpos($className, Cloudinary_Cloudinary_Model_Observer::CLOUDINARY_EXTENSION_LIB_PATH . '\\') === 0 ||
                     strpos($className, Cloudinary_Cloudinary_Model_Observer::CLOUDINARY_LIB_PATH . '\\') === 0
                 ) {
-                    include_once preg_replace('#\\\|_(?!.*\\\)#', '/', $className) . '.php';
+                    include_once preg_replace(self::CONVERT_CLASS_TO_PATH_REGEX, '/', $className) . '.php';
                 }
             }
         );
