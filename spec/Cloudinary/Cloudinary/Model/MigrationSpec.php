5fe6a48bebb9feae16354d9e80bf24696d2af2f5
Jon Acker <jacker@inviqa.com>
2015-02-23 14:45:37 +0000

    Example of mage style class specs

---
+++ spec/Cloudinary/Cloudinary/Model/MigrationSpec.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace spec;
+
+use PhpSpec\ObjectBehavior;
+
+class Cloudinary_Cloudinary_Model_MigrationSpec extends ObjectBehavior
+{
+    function it_is_a_something() {
+        $this->shouldHaveType('Cloudinary_Cloudinary_Model_Migration');
+    }
+}
