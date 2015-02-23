d6e484d3ac0b5f7d7500bdb52cd945e59514231e
David Herbert <dherbert@sessiondigital.com>
2015-02-23 16:04:14 +0000

    SESSCLOUD-76: Amend spec for Migration task

--- spec/Cloudinary/Cloudinary/Model/MigrationSpec.php
+++ spec/Cloudinary/Cloudinary/Model/MigrationSpec.php
@@ -6,7 +6,8 @@ use PhpSpec\ObjectBehavior;
 
 class Cloudinary_Cloudinary_Model_MigrationSpec extends ObjectBehavior
 {
-    function it_is_a_something() {
-        $this->shouldHaveType('Cloudinary_Cloudinary_Model_Migration');
+    function it_should_be_a_migration_task()
+    {
+        $this->shouldHaveType('CloudinaryExtension\Migration\Task');
     }
 }
