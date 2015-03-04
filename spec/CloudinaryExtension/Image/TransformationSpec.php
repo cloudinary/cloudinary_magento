0a99be55769895e6831fbd6f191e15284355aa1f
Jon Acker <jacker@inviqa.com>
2015-03-04 09:44:39 +0000

    Clarify spec

--- spec/CloudinaryExtension/Image/TransformationSpec.php
+++ spec/CloudinaryExtension/Image/TransformationSpec.php
@@ -9,22 +9,19 @@ use Prophecy\Argument;
 
 class TransformationSpec extends ObjectBehavior
 {
-    function it_has_no_gravity_value_by_default()
+    function it_builds_with_dimensions()
     {
-        $this->getGravity()->shouldBe(null);
-    }
-
-    function it_can_have_dimensions()
-    {
-        $this->withDimensions(Dimensions::fromWidthAndHeight(10, 10));
+        $transformation = $this->withDimensions(Dimensions::fromWidthAndHeight(10, 10));
 
         $this->getDimensions()->shouldBeLike(Dimensions::fromWidthAndHeight(10, 10));
+        $transformation->shouldBe($this);
     }
 
-    function it_can_have_gravity()
+    function it_builds_with_gravity()
     {
-        $this->withGravity(Gravity::fromString('center'));
+        $transformation = $this->withGravity(Gravity::fromString('center'));
 
         $this->getGravity()->shouldBeLike(Gravity::fromString('center'));
+        $transformation->shouldBe($this);
     }
 }
