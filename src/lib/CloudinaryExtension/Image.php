aec44784f9c5d5b6fd434bb0c3c2be9303fd9727
Attila Neparaczki <aneparaczki@sessiondigital.com>
2015-08-27 17:38:42 +0200

    relativePath property to Image

--- src/lib/CloudinaryExtension/Image.php
+++ src/lib/CloudinaryExtension/Image.php
@@ -5,23 +5,29 @@ namespace CloudinaryExtension;
 class Image
 {
     private $imagePath;
-
+    private $relativePath;
     private $pathParts;
 
-    private function __construct($imagePath)
+    private function __construct($imagePath, $relativePath = '')
     {
         $this->imagePath = $imagePath;
+        $this->relativePath = $relativePath;
         $this->pathParts = pathinfo(basename($this->imagePath));
     }
 
-    public static function fromPath($anImagePath)
+    public static function fromPath($anImagePath, $relativepath = '')
     {
-        return new Image($anImagePath);
+        return new Image($anImagePath, $relativepath);
     }
 
     public function __toString()
     {
         return $this->imagePath;
+    }
+
+    public function getRelativePath()
+    {
+        return $this->relativePath;
     }
 
     public function getId()
