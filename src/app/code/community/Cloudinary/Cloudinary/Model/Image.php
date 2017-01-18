b3e8d863be4a9d5a33e0177331945955a7d49423
Rick Peacock <rpeacock@inviqa.com>
2017-01-18 16:05:27 +0000

    SESSCLOUD-419 Remove folder reference during delete operation
    This change prevents the image from having a relative path when in non-foldered mode.
    This prevents an unwanted forward slash when the SDK is called that causes the
    delete operation to fail.
    This could be implemented by refactoring but that will take considerably more effort
    to ensure no current features are effected.

--- src/app/code/community/Cloudinary/Cloudinary/Model/Image.php
+++ src/app/code/community/Cloudinary/Cloudinary/Model/Image.php
@@ -4,18 +4,20 @@ use CloudinaryExtension\CloudinaryImageProvider;
 use CloudinaryExtension\Image;
 use Cloudinary_Cloudinary_Model_Exception_BadFilePathException as BadFilePathException;
 
-
 class Cloudinary_Cloudinary_Model_Image extends Mage_Core_Model_Abstract
 {
+    /**
+     * @param array $imageDetails
+     */
     public function upload(array $imageDetails)
     {
         $configuration = Mage::getModel('cloudinary_cloudinary/configuration');
-        $imageManager = CloudinaryImageProvider::fromConfiguration($configuration);
+        $imageProvider = CloudinaryImageProvider::fromConfiguration($configuration);
 
         $fullPath = $this->_imageFullPathFromImageDetails($imageDetails);
         $relativePath = $configuration->isFolderedMigration() ? $configuration->getMigratedPath($fullPath) : '';
 
-        $imageManager->upload(Image::fromPath($fullPath, $relativePath));
+        $imageProvider->upload(Image::fromPath($fullPath, $relativePath));
 
         Mage::getModel('cloudinary_cloudinary/synchronisation')
             ->setValueId($imageDetails['value_id'])
@@ -23,11 +25,22 @@ class Cloudinary_Cloudinary_Model_Image extends Mage_Core_Model_Abstract
             ->tagAsSynchronized();
     }
 
-    private function _imageFullPathFromImageDetails($imageDetails)
+    /**
+     * @param array $imageDetails
+     * @return string
+     * @throws Cloudinary_Cloudinary_Model_Exception_BadFilePathException
+     */
+    private function _imageFullPathFromImageDetails(array $imageDetails)
     {
         return  $this->_getMediaBasePath() . $this->_getImageDetailFromKey($imageDetails, 'file');
     }
 
+    /**
+     * @param array $imageDetails
+     * @param string $key
+     * @return string
+     * @throws Cloudinary_Cloudinary_Model_Exception_BadFilePathException
+     */
     private function _getImageDetailFromKey(array $imageDetails, $key)
     {
         if (!array_key_exists($key, $imageDetails)) {
@@ -36,8 +49,35 @@ class Cloudinary_Cloudinary_Model_Image extends Mage_Core_Model_Abstract
         return $imageDetails[$key];
     }
 
+    /**
+     * @return string
+     */
     private function _getMediaBasePath()
     {
         return Mage::getSingleton('catalog/product_media_config')->getBaseMediaPath();
+    }
+
+    /**
+     * @param string $imageName
+     */
+    public function deleteImage($imageName)
+    {
+        $configuration = Mage::getModel('cloudinary_cloudinary/configuration');
+        $imageProvider = CloudinaryImageProvider::fromConfiguration($configuration);
+        $migratedPath = $configuration->isFolderedMigration() ? $configuration->getMigratedPath($imageName) : '';
+        $imageProvider->deleteImage(Image::fromPath($imageName, ltrim($migratedPath, '/')));
+    }
+
+    /**
+     * @param string $imagePath
+     * @return string
+     */
+    public function getUrl($imagePath)
+    {
+        $imageProvider = CloudinaryImageProvider::fromConfiguration(
+            Mage::getModel('cloudinary_cloudinary/configuration')
+        );
+
+        return (string)$imageProvider->transformImage(Cloudinary_Cloudinary_Helper_Image::newApiImage($imagePath));
     }
 }
