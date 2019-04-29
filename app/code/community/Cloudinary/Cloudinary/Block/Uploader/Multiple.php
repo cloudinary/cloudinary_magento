64b86259800901276ea9c7b89a277628f61252c3
pini-girit <pini@girit.biz>
2019-04-29 10:04:41 +0300

    CLOUDINARY-127: Working on media-library implementation (IN PROGRESS)

---
+++ app/code/community/Cloudinary/Cloudinary/Block/Uploader/Multiple.php
@@ -0,0 +1,143 @@
+<?php
+
+class Cloudinary_Cloudinary_Block_Uploader_Multiple extends Mage_Uploader_Block_Multiple
+{
+    /**
+     * Default browse button ID suffix
+     */
+    const DEFAULT_CLD_ML_BUTTON_ID_SUFFIX = 'cld_ml';
+
+    /**
+     * Template used for uploader
+     *
+     * @var string
+     */
+    protected $_template = 'cloudinary/media/uploader.phtml';
+
+    /**
+     * Prepare layout, create buttons, set front-end elements ids
+     *
+     * @return Mage_Core_Block_Abstract
+     */
+    protected function _prepareLayout()
+    {
+        parent::_prepareLayout();
+
+        $this->setChild(
+            'cloudinary_ml_button',
+            $this->getLayout()->createBlock('adminhtml/widget_button')
+                ->addData(array(
+                    // Workaround for IE9
+                    'before_html'   => sprintf(
+                        '<div style="display:inline-block;" id="%s">',
+                        $this->getElementId(self::DEFAULT_CLD_ML_BUTTON_ID_SUFFIX)
+                    ),
+                    'after_html'    => '</div>
+                        <script type="text/javascript">
+                        //<![CDATA[
+                        var ' . self::DEFAULT_CLD_ML_BUTTON_ID_SUFFIX . '_instance_' . $this->getHtmlId() . ' = new CloudinaryMediaLibrary(' . $this->getCloudinaryMediaLibraryWidgetOptions() . ');
+                        //]]>
+                        </script>',
+                    'id'            => $this->getElementId(self::DEFAULT_CLD_ML_BUTTON_ID_SUFFIX . '_button'),
+                    'label'         => Mage::helper('uploader')->__('Add From Cloudinary...'),
+                    'type'          => 'button',
+                ))
+        );
+
+        return $this;
+    }
+
+    /**
+     * Get CLD ML button html
+     *
+     * @return string
+     */
+    public function getCldMLButtonHtml()
+    {
+        return $this->getChildHtml('cloudinary_ml_button');
+    }
+
+    /**
+     * Get Cloudinary media library widget options
+     *
+     * @param bool $multiple Allow multiple
+     * @param bool $refresh Refresh options
+     * @return string
+     */
+    public function getCloudinaryMediaLibraryWidgetOptions($multiple = true, $refresh = false)
+    {
+        if (!($cloudinaryMLoptions = Mage::helper('cloudinary_cloudinary/MediaLibraryHelper')->getCloudinaryMLOptions($multiple, $refresh))) {
+            return null;
+        }
+        return Mage::helper('core')->jsonEncode(array(
+            'htmlId' => $this->getHtmlId(),
+            'cldMLid' => self::DEFAULT_CLD_ML_BUTTON_ID_SUFFIX . '_' . $this->getHtmlId(),
+            'imageUploaderUrl' => $this->getCldImageUploaderUrl(),
+            'buttonSelector' => '#' . $this->getElementId(self::DEFAULT_CLD_ML_BUTTON_ID_SUFFIX . '_button'),
+            'triggerSelector' => $this->getTriggerSelector(),
+            'triggerEvent' => $this->getTriggerEvent(),
+            'callbackHandler' => $this->getCallbackHandler(),
+            'callbackHandlerMethod' => $this->getCallbackHandlerMethod(),
+            'useDerived' => $this->getUseDerived(),
+            'addTmpExtension' => $this->getAddTmpExtension(),
+            'cloudinaryMLoptions' => $cloudinaryMLoptions,
+            'cloudinaryMLshowOptions' => Mage::helper('cloudinary_cloudinary/MediaLibraryHelper')->getCloudinaryMLshowOptions('image'),
+        ));
+    }
+
+    /**
+     * @return string
+     */
+    protected function getCldImageUploaderUrl()
+    {
+        return Mage::getModel('adminhtml/url')->addSessionParam()->getUrl('*/cloudinaryretrieveimage/upload');
+    }
+
+    /**
+     * @return bool
+     */
+    protected function getAddTmpExtension()
+    {
+        return true;
+    }
+
+    /**
+     * @return bool
+     */
+    protected function getUseDerived()
+    {
+        return false;
+    }
+
+    /**
+     * @return mixed
+     */
+    protected function getTriggerEvent()
+    {
+        return 'addItem';
+    }
+
+    /**
+     * @return mixed
+     */
+    protected function getTriggerSelector()
+    {
+        return 'triggerSelector';
+    }
+
+    /**
+     * @return mixed
+     */
+    protected function getCallbackHandler()
+    {
+        return 'window.media_gallery_contentJsObject';
+    }
+
+    /**
+     * @return mixed
+     */
+    protected function getCallbackHandlerMethod()
+    {
+        return 'handleUploadComplete';
+    }
+}
--- app/code/community/Cloudinary/Cloudinary/Helper/Data.php
+++ app/code/community/Cloudinary/Cloudinary/Helper/Data.php
@@ -2,5 +2,21 @@
 
 class Cloudinary_Cloudinary_Helper_Data extends Mage_Core_Helper_Abstract
 {
-
+    public static function curlGetContents($url)
+    {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_VERBOSE, 1);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+        $result = curl_exec($ch);
+        $httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        $err = curl_error($ch);
+        curl_close($ch);
+        return new Varien_Object(array(
+            "code" => $httpCode,
+            "body" => $result,
+            "error" => $err
+        ));
+    }
 }
